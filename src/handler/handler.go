f7169f758f339126951e06bc27845049aa747f94
BigTony <kejiezhang666@gmail.com>
2019-04-27 16:42:02 -0400

    feat: add upload GET

--- src/handler/handler.go
+++ src/handler/handler.go
@@ -1,9 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 /**
@@ -19,5 +21,33 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, string(data))
 	} else if r.Method == "POST" {
 		// acept file stream and store to local
+		file, head, err := r.FormFile("file")
+		if err != nil {
+			fmt.Print("Failed to get data, err: %s", err.Error())
+			return
+		}
+		defer file.Close()
+
+		newFile, err := os.Create("../tmp/" + head.Filename)
+		if err != nil {
+			fmt.Print("Failed to create file, err:%s\n", err.Error())
+			return
+		}
+		defer newFile.Close()
+
+		_, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Print("Failed to save data into file, err: %s", err.Error())
+			return
+		}
+
+		http.Redirect(w, r, "/file/upload/success", http.StatusFound)
 	}
+}
+
+/**
+ * Upload Success Handler
+ */
+func UploadSuccessHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload finished")
 }
--- src/main.go
+++ src/main.go
@@ -9,6 +9,8 @@ import (
 
 func main() {
 	http.HandleFunc("/file/upload", handler.UploadHandler)
+	http.HandleFunc("/file/upload/success", handler.UploadSuccessHandler)
+
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Print("Failed to start server, err: %s", err.Error())
