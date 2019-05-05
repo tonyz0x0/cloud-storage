0e3172a2fe9e223f13657aa73d7619ef3e74f55e
BigTony <kejiezhang666@gmail.com>
2019-05-05 14:25:40 +0800

    feat: add multipart upload

---
+++ cache/redis/conn.go
@@ -0,0 +1,53 @@
+package redis
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gomodule/redigo/redis"
+)
+
+var (
+	pool      *redis.Pool
+	redisHost = "127.0.0.1:6379"
+	redisPass = "testupload"
+)
+
+// newRedisPool: Create Redis Pool
+func newRedisPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:     50,
+		MaxActive:   30,
+		IdleTimeout: 300 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			// Open Connection
+			c, err := redis.Dial("tcp", redisHost)
+			if err != nil {
+				fmt.Println(err)
+				return nil, err
+			}
+
+			// Verification
+			if _, err := c.Do("AUTH", redisPass); err != nil {
+				c.Close()
+				return nil, err
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(conn redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := conn.Do("PING")
+			return err
+		},
+	}
+}
+
+func init() {
+	pool = newRedisPool()
+}
+
+func RedisPool() *redis.Pool {
+	return pool
+}
--- src/main.go
+++ src/main.go
@@ -20,10 +20,20 @@ func main() {
 	http.HandleFunc("/file/download", handler.DownloadHandler)
 	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
 	http.HandleFunc("/file/delete", handler.FileDeleteHandler)
-	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(
-		handler.TryFastUploadHandler,
-	))
 
+	// XSpeed Upload
+	http.HandleFunc("/file/fastupload", handler.HTTPInterceptor(
+		handler.TryFastUploadHandler))
+
+	// Multipart Upload
+	http.HandleFunc("/file/mpupload/init",
+		handler.HTTPInterceptor(handler.InitialMultipartUploadHandler))
+	http.HandleFunc("/file/mpupload/uppart",
+		handler.HTTPInterceptor(handler.UploadPartHandler))
+	http.HandleFunc("/file/mpupload/complete",
+		handler.HTTPInterceptor(handler.CompleteUploadHandler))
+
+	// User Interface
 	http.HandleFunc("/user/signup", handler.SignUpHandler)
 	http.HandleFunc("/user/signin", handler.SignInHandler)
 	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
